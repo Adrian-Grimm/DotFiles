035ea4a03f3792ce51a8febc576d23376ea1424e
Adrian-Grimm <adrian@DESVC-AGM-10.ce.sdxcorp.net>
2019-07-24 16:23:34 +0200

    Tilix Windows WSL Start for X410.exe

--- Tilix_WSL-Launcher.zsh
+++ Tilix_WSL-Launcher.zsh
@@ -1,3 +1,5 @@
 #bin! zsh
 Powershell.exe -Command 'start X410.exe /WM'
-Display=:0 dbus-launch --exit-with-session tilix&
+
+tmux new-session -s "Tilix" -n "Console" -d&
+tmux send-keys -t Tilix "Display=:0 dbus-launch --exit-with-session tilix&" C-m
