e37ddeeaee1d6ec85a6823083f7ef05e7e08c0f8
Adrian-Grimm <adrian@DESKTOP-GSPJ790.localdomain>
2020-02-09 20:41:58 +0100

    HighRes Mode comment in Tilix-Launcher script.

--- Tilix_WSL-Launcher.zsh
+++ Tilix_WSL-Launcher.zsh
@@ -3,6 +3,11 @@
 Powershell.exe -Command 'start X410.exe /WM'
 
 tmux new-session -s "Tilix" -n "Console" -d&
+
+#Enable High-DPI Support:
+# tmux send-keys -t Tilix "export QT_SCALE_FACTOR=2" C-m
+# tmux send-keys -t Tilix "export GDK_SCALE=2" C-m
+
 tmux send-keys -t Tilix "Display=:0 dbus-launch --exit-with-session tilix&" C-m
 
 # Create a Link in Windows with:
